afe84c9a79f08e15ad83ea2ad20df0f73358320f
ChenLin12138 <scorpion_chenlin@163.com>
2020-04-26 12:04:38 +0800

    修改配置文件，添加一些学注释

--- nicefish-cms/src/main/java/com/nicefish/cms/controller/PostController.java
+++ nicefish-cms/src/main/java/com/nicefish/cms/controller/PostController.java
@@ -1,20 +1,26 @@
 package com.nicefish.cms.controller;
 
-import com.nicefish.auth.shiro.util.ShiroUtil;
-import com.nicefish.cms.jpa.entity.PostEntity;
-import com.nicefish.cms.service.IPostService;
-import com.nicefish.core.web.AjaxResult;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import lombok.extern.slf4j.Slf4j;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpSession;
+import com.nicefish.auth.shiro.util.ShiroUtil;
+import com.nicefish.cms.jpa.entity.PostEntity;
+import com.nicefish.cms.service.IPostService;
+import com.nicefish.core.web.AjaxResult;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @author 大漠穷秋
@@ -32,8 +38,9 @@ public class PostController {
     @ApiOperation("获取文章列表，按时间倒排，带分页")
     @RequestMapping(value = "/post-list/{page}", method = RequestMethod.GET)
     public Page<PostEntity> getPostList(@PathVariable(value="page",required = false) Integer page) {
-        Pageable pageable= PageRequest.of(page-1,10, new Sort(Sort.Direction.DESC,"postId"));
-
+    	//利用spring data pageablez进行分页。
+    	//PageRequest.of(第几页，一页多少个，排序方式)
+    	Pageable pageable= PageRequest.of(page-1,10, new Sort(Sort.Direction.DESC,"postId"));
         return postService.getPostsPaging(pageable);
     }
 
